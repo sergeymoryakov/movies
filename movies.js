b42f9c90a34777e3d132d7b7144070ff6b9d5a5a
Sergey Moryakov <sergey@moryakov.com>
2023-06-28 15:43:50 +0300

    Set basic search and rendering

--- movies.js
+++ movies.js
@@ -1,6 +1,6 @@
 const movieSearchNode = document.getElementById("movieSearch");
 const movieSearchBtnNode = document.getElementById("movieSearchBtn");
-const searchResultNode = document.getElementById("searchResult");
+let searchResultNode = document.getElementById("searchResult");
 
 movieSearchBtnNode.addEventListener("click", function () {
     const movieName = movieSearchNode.value;
@@ -12,6 +12,41 @@ movieSearchBtnNode.addEventListener("click", function () {
             .then((resp) => resp.json())
             .then((data) => {
                 console.log(data);
+                renderSearchResult(data);
             });
     }
 });
+
+function renderSearchResult(result) {
+    let htmlCode = "";
+    searchResultNode.innerHTML = "";
+
+    result.Search.forEach((item) => {
+        // ATTN: FOR DEV ONLY - REMOVE IN PRODUCTION
+        // console.log(item.Poster);
+        // console.log(item.Title);
+        // console.log(item.Type);
+        // console.log(item.Year);
+        // console.log(item.imdbID);
+
+        htmlCode += `
+        <li class="movie-search__card_wrapper">
+                    <div class="movie-search__card_poster-wrapper">
+                        <img
+                            class="movie-search__card_poster"
+                            src="${item.Poster}"
+                            alt="No Poster"
+                        />
+                    </div>
+                    <div class="movie-search__card_description-wrapper">
+                        <h3 class="movie-search__card_title">
+                            ${item.Title}
+                        </h3>
+                        <p class="movie-search__card_year">${item.Year}</p>
+                        <p class="movie-search__card_type">${item.Type}</p>
+                    </div>
+                </li>
+        `;
+    });
+    searchResultNode.innerHTML = htmlCode;
+}
